2e54c7f7d4d16fbe92df765a4fbceca4debff648
danya7777 <mishindaniil365247@gmail.com>
2023-05-02 18:34:31 +0500

    Changes to be committed: 	modified:   main 	modified:   main.c

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    for(int i = 0; i < 5; i++) {
+    for(int i =1;i<=25;i++){
         printf("*");
-    }
-    for(int i = 0;i<3;i++) {
-        printf("\n*");
-        for(int j = 0;j<3;j++) {
-            printf(" ");
+        if(i==5||i==10||i==15||i==20) {
+            printf("\n");
         }
-        printf("*");
-    }
-    printf("\n");
-        for(int i = 0; i < 5; i++) {
-        printf("*");
     }
     return 0;
 }
